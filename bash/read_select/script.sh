18849cf5f7039560d69e1e996a2994e4fc21b549
sadiq_from_latitude <sadiqonemail@gmail.com>
2025-08-02 14:29:29 +0530

    select

--- bash/read_select/script.sh
+++ bash/read_select/script.sh
@@ -1,3 +1,12 @@
 
 read -p "Enter your name: " name
 echo "Hello, $name!"
+
+
+select option in Start Stop Exit; do
+  case $option in
+    Start) echo "Starting...";;
+    Stop) echo "Stopping...";;
+    Exit) break;;
+  esac
+done
