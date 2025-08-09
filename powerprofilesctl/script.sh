e4ee85bbb6bebd22d679812c9b875687cb749f33
sadiq_from_latitude <sadiqonemail@gmail.com>
2025-08-09 18:46:09 +0530

    powerprofilesctl

---
+++ powerprofilesctl/script.sh
@@ -0,0 +1,10 @@
+
+
+# Make power profiles handling available over D-Bus.
+# More information: https://gitlab.freedesktop.org/upower/power-profiles-daemon#how-to-use.
+
+ # - List available power profiles:
+powerprofilesctl list
+
+ # - Set a specific power profile:
+powerprofilesctl set profile_name
