1dd0a9e3a1daf04dd0835b6734e1574d91deb274
sadiq_from_latitude <sadiqonemail@gmail.com>
2025-08-29 07:09:46 +0530

    tmux added

---
+++ tmux/script.sh
@@ -0,0 +1,10 @@
+# create new session inside a new session
+:new-session -s mysession
+
+# find key associated with a cmd
+tmux list-keys | grep new-session
+tmux list-keys -N  | grep session
+
+# find cmd associated with a key
+list-keys <key>
+pre + /
