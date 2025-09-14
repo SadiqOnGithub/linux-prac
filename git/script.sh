3b40092150602c6b53aebdb78be9c8a3eea8d91c
sadiq_from_latitude <sadiqonemail@gmail.com>
2025-09-15 04:18:56 +0530

    git more cmds added

--- git/script.sh
+++ git/script.sh
@@ -33,5 +33,15 @@ git log -G "regex_pattern" --oneline  # Using regex
 
 git config --list
 git config --global --list
+git config --global --unset property_name
+git config --show-origin property_name
+git config --show-scope property_name
 
-git config --global --unset <property_name>
+
+git config --global fetch.prune true
+
+git branch --merged
+git pull --prune
+
+git config --global core.editor "vim"
+git config --show-origin core.editor
