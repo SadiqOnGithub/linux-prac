0d8c35d38b6a06d5521a956b5ca1f20c547354ef
sadiq_from_thinkpad <sadiqonemail@gmail.com>
2025-09-26 21:26:59 +0530

    git ref HEAD and froce branch move

--- git/script.sh
+++ git/script.sh
@@ -19,3 +19,12 @@ git pull --prune
 git push origin --delete xyz-branch
 
 # origin vs upstream in forking
+
+# detached HEAD can be on same commit as the branch is.
+# https://www.reddit.com/r/git/comments/1nqu887/git_people_spot_the_difference/
+
+# https://x.com/SyedSadiqOnX/status/1971569752599605672
+git checkout HEAD^^^^^^
+
+# moves (by force) the main branch to three parents behind HEAD.
+git branch -f main HEAD^^^
