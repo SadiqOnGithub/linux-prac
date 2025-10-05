9685228918efee3e67278a1667c3353627c77b64
sadiq_from_thinkpad <sadiqonemail@gmail.com>
2025-10-06 00:47:34 +0530

    docker added with some terraform config

---
+++ docker/terraform.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.2"
+    }
+  }
+  required_version = "~> 1.7"
+}
+
+# deploying docker image locally?
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "docker.mirror.hashicorp.services/nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
