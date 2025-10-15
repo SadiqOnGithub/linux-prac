b87fbfb639afc63c73994157ccb14abe00804f48
sadiq_from_thinkpad <sadiqonemail@gmail.com>
2025-10-15 21:45:25 +0530

    tailscale added

---
+++ tailscale/script.sh
@@ -0,0 +1,12 @@
+
+# start tailscale
+tailscale up
+
+# stop tailscale
+tailscale down
+
+# status tailscale : --flags: --web, --json, --active
+tailscale status [--flags]
+
+# Print an analysis of local network conditions
+tailscale netcheck
