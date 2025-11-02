f6929e61cb0175c338daebae75b055e0b47c4377
sadiq_from_thinkpad <sadiqonemail@gmail.com>
2025-11-02 12:32:15 +0530

    rust: tuples

--- rust/concepts.rs
+++ rust/concepts.rs
@@ -4,4 +4,9 @@ fn main() {
 
     println!("Array: {:?}", arr_constructor);
     println!("Array: {:?}", arr);
+
+    // =========================
+    let t = ([1; 2], [3; 4]);
+    let (a, _b) = t;
+    println!("{}", a[0] + t.1[0]);
 }
